aeacca8aa28a8b1a4c89c4d6cee7ec0daf6fb53c
kimnamhyeon0112 <knh306@naver.com>
2024-05-20 15:28:08 +0900

    분석페이지 추가 및 연결작업

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def summary(request):
+    return render(request, 'summary.html', {})
 # Create your views here.
