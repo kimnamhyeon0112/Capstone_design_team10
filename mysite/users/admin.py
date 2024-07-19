0066ed08c66030fcb616dbb322e32c59cc10c609
songwon Lee <songwon12495@gmail.com>
2024-07-19 16:06:51 +0900

    회원가입 기능 구현

--- mysite/users/admin.py
+++ mysite/users/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-# Register your models here.
-admin.site.register(User, UserAdmin)
+# Django's built-in user model requires user ID, as opposed to what we're trying to do
+# Changing the custom model means having to implement custom admin as well
+# https://tech.serhatteker.com/post/2020-01/email-as-username-django/
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(User, CustomUserAdmin)
