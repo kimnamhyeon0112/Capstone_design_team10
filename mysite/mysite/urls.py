ac396d1b7e26927aab427c810bd23125a3b858b1
SChoi1828 <146840997+SChoi1828@users.noreply.github.com>
2024-10-07 22:01:48 +0900

    Enable language prefix (/en/, /ko/) to switch language

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 from django.urls import include, path
 from home.views import home
 from django.contrib.auth import views as auth_views
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('users/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('', home),
     path('/', include('home.urls')),
-    path('', include('users.urls'))
-]
+    path('', include('users.urls')),
+    prefix_default_language=False
+)
